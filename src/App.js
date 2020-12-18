9cf7e665d7eb584b717abf99166209f4c185809a
Ahmad Naji <Survivor@Survivor>
2020-12-18 17:50:15 +0200

    check works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-// import logo from './logo.svg';
 import React from "react";
-import TodoList from "./Checkbox";
+import TodoItem from "./TodoItem";
 import todosData from "./todosData";
-import "./App.css";
-
-// function App() {
-//   const list = todosData.map((elt) => {
-//     return <TodoList key={elt.id} task={elt.text} elt={elt} />;
-//   });
-
-//   return <div className="todo-list">{list}</div>;
-// }
 
 class App extends React.Component {
-  render(){
-    const list = todosData.map((elt) => {
-          return <TodoList key={elt.id} task={elt.text} elt={elt} />;
-        });
-      
-        return <div className="todo-list">{list}</div>;
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) todo.completed = !todo.completed;
+        return todo;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
   }
 }
 
